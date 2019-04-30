104e124e51efb2d0fed7b931c6b481d8f914f0e4
tryingtokeepup <kl.hawaii@gmail.com>
2019-04-29 21:07:40 -0700

    man, this was rough. thanks james. MVP is done.

--- arrays/james_array.c
+++ arrays/james_array.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <stdbool.h>
+#include <string.h>
+
 typedef struct Array
 {
-  int capacity;    // How many elements can this array hold?
-  int count;       // How many states does the array currently hold?
+  int capacity;    // How many elements can this array hold? It will hold the amount passed in as the arg upon creation
+  int count;       // How many states does the array currently hold? as many as **elements is not null
   char **elements; // The string elements contained in the array
 } Array;
 
@@ -41,17 +42,14 @@ void destroy_array(Array *arr)
 {
 
   // Free all elements
-  for (int i = 0; i < arr->capacity; i++)
+  for (int i = arr->count - 1; i >= 0; i--)
   {
-    //free(arr->elements[i]);
-    printf("element: %s \n", arr->elements[i]);
     arr->elements[i] = NULL;
     free(arr->elements[i]);
-    printf("maybe we were cool \n");
   }
-  // // security freak James Page recommends nulling out all pointers and refs for security
-  // arr->elements = NULL
+  arr->elements = NULL;
   free(arr->elements);
+
   // Free array
   free(arr);
 }
@@ -62,23 +60,26 @@ void destroy_array(Array *arr)
  *****/
 void resize_array(Array *arr)
 {
-  arr->capacity *= 2;
-  // Create a new element storage with double capacity
-  char **temp = malloc(arr->capacity * sizeof(char *));
-  // Copy elements into the new storage
+  // other solution
+  // arr->capacity *= 2;
+  // arr->elements = realloc(arr->elements, (arr->capacity *) * sizeof(char *)); // already doubled capacity, so we don't need *2
 
-  int i = 0;
-  while (i < arr->count) //i don't want to hit the null pointer
+  // Create a new element storage with double capacity <- confusing wording
+  // Increase the capacity to double size              <-p what I thin it means
+  arr->capacity *= 2;
+  char **storage = malloc(arr->capacity * sizeof(char *));
+
+  // Copy elements into the new storage
+  for (int i = 0; i < arr->count; i++)
   {
-    temp[i] = arr->elements[i];
-    i++;
+    storage[i] = arr->elements[i];
   }
 
   // Free the old elements array (but NOT the strings they point to)
   free(arr->elements);
+
   // Update the elements and capacity to new values
-  arr->elements = temp;
-  // already ... umm... resized arr->capacity
+  arr->elements = storage;
 }
 
 /************************************
@@ -94,14 +95,13 @@ void resize_array(Array *arr)
  *****/
 char *arr_read(Array *arr, int index)
 {
-
   // Throw an error if the index is greater or equal to than the current count
-  // you can hit the null character, no biggie, because you disregard it anyway?
+  // Throw a NULL if the index is greater or equal to than the current count <- needs to be updated to test case
   if (index >= arr->count)
   {
-    //printf("the index is out of range, check it real quick.\n");
-    return NULL;
+    return '\0';
   }
+
   // Otherwise, return the element at the given index
   return arr->elements[index];
 }
@@ -111,33 +111,36 @@ char *arr_read(Array *arr, int index)
  *****/
 void arr_insert(Array *arr, char *element, int index)
 {
-
   // Throw an error if the index is greater than the current count
   if (index > arr->count)
   {
-    printf("out of bounds, check your index please!\n");
-    return; // what do we return on a void?
+    printf("index out of bounds\n");
+    return;
   }
+
   // Resize the array if the number of elements is over capacity
-  // when would we hit this? the count will always match the capacity.
+  int orig_capacity = arr->capacity;
   if (arr->count == arr->capacity)
-  // if we have a count that is equal to the capacity, we need to allocate a lot more space for it
   {
-    printf("ooh, we need to resize. \n");
     resize_array(arr);
+    if (orig_capacity == arr->capacity)
+    {
+      printf("array is full and cannot be resized\n");
+      return;
+    }
   }
-  // Move every element after the insert index to the right by one position
-  // so disappointed, but i guess i have to iterate down
+
+  // Move every element after the insert index to the right one position
   for (int i = arr->count - 1; i >= index; i--)
   {
-    printf("loop count %d", i);
-
     arr->elements[i + 1] = arr->elements[i];
   }
+
   // Copy the element and add it to the array
   arr->elements[index] = strdup(element);
+
   // Increment count by 1
-  arr->count += 1;
+  arr->count++;
 }
 
 /*****
@@ -145,17 +148,26 @@ void arr_insert(Array *arr, char *element, int index)
  *****/
 void arr_append(Array *arr, char *element)
 {
-
   // Resize the array if the number of elements is over capacity
   // or throw an error if resize isn't implemented yet.
-  if (arr->capacity == arr->count)
+  int orig_capacity = arr->capacity;
+  if (arr->count == arr->capacity)
   {
     resize_array(arr);
+    if (orig_capacity == arr->capacity)
+    {
+      printf("%d", orig_capacity);
+      printf("%d", arr->capacity);
+      printf("array is full and cannot be resized\n");
+      return;
+    }
   }
+
   // Copy the element and add it to the end of the array
   arr->elements[arr->count] = strdup(element);
+
   // Increment count by 1
-  arr->count += 1;
+  arr->count++;
 }
 
 /*****
@@ -166,27 +178,30 @@ void arr_append(Array *arr, char *element)
  *****/
 void arr_remove(Array *arr, char *element)
 {
-  int i = 0;
-  bool found = false;
   // Search for the first occurence of the element and remove it.
-  for (; i < arr->count && !found; i++)
+  int i = 0;
+  while (strcmp(arr->elements[i], element) != 0 && i < arr->count) // O(k)
   {
-    if (strcmp(arr->elements[i], element) == 0)
-    // if strcmp returns a 0, that means we found a match!
-    {
-      found = true;
-      arr->elements[i] = NULL;
-      i--;
-      //free(arr->elements[i]);
-    }
+    i++;
   }
+
+  // if element was not found, return error
+  if (i == arr->count)
+  {
+    printf("element not found in array\n");
+    return;
+  }
+
   // Don't forget to free its memory!
-  // i did daddy
+  arr->elements[i] = NULL;
+  free(arr->elements[i]);
+
   // Shift over every element after the removed element to the left one position
-  for (; i < arr->count - 1; i++)
+  for (; i < arr->count - 1; i++) // O(k)
   {
     arr->elements[i] = arr->elements[i + 1];
   }
+  // remove duplicate at end, note: i is already at end from final i++ in for loop, or while loop at worst case
   arr->elements[i] = NULL;
   free(arr->elements[i]);
 
